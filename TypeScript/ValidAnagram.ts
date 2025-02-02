a82a56913eaa4078c7c629f348771b76a431d5dd
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2025-02-02 18:43:17 +0200

    Add new file ValidAnagram.ts, update README.md, and remove isAnagram.ts

--- TypeScript/ValidAnagram.ts
+++ TypeScript/ValidAnagram.ts
@@ -1,3 +1,29 @@
+/*
+ Given two strings s and t, return true if t is an
+anagram
+ of s, and false otherwise.
+
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+
+Output: false
+
+
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.*/
+
 function isAnagram(s: string, t: string): boolean {
     if (s.length !== t.length)
         return false;
