5599ea0ea1fe3eaa78b7862fb656ced1748bc4ca
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2025-02-21 11:23:44 +0200

    Update TwoSum.js

--- JavaScript/TwoSum.js
+++ JavaScript/TwoSum.js
@@ -11,3 +11,20 @@ var twoSum = function(nums, target) {
         }
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const compliment = target - nums[i]
+    if (map.has(compliment)){
+        return [map.get(compliment), i];
+    }
+    map.set(nums[i], i)
+  }
+  return []
+}
